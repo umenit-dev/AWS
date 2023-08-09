e0752b130b2d5c392d662d34f98f7d942a114637
vinnethvicky <gazzela.vivakar@gmail.com>
2023-08-09 21:44:11 +0530

    added terraform file in folder new

---
+++ AWS/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags= {
+    Name = var.tag_name
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
+  }
+}
